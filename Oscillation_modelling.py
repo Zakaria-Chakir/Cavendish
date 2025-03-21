148e17530f3e90aa94881ec7b08820275b01080a
Zakaria-Chakir <zakaria.chakir@mail.mcgill.ca>
2025-03-20 23:57:12 -0400

    Small changes with some debugging

--- Oscillation_modelling.py
+++ Oscillation_modelling.py
@@ -4,19 +4,16 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 
-def torsional_oscillator_model(x, C, damp, I, kappa, G, phi):
+def torsional_oscillator_model(x, C, damp, I, kappa, G, phi, m_small, dist_mass, d, k):
     # Known values (might be subject to changes)
-    m_small = 15*10**-3
     m_big = 1.5
-    dist_mass = 0.01
-    d = 6
 
     # Actual model
     b = 2*damp/I 
     kappa_eff = kappa + 2*G*d*((m_small*m_big)/(dist_mass))
     w0 = np.sqrt(kappa_eff/I)
     w1 = np.sqrt(w0**2 - b**2)
-    theta = [C*np.e**(-b*t)* np.cos(w1*t+phi) for t in x]
+    theta = [C*np.e**(-b*t)* np.cos(w1*t+phi) + k for t in x]
     return theta 
 
 def rnd_color(n):
@@ -40,13 +37,13 @@ def curve_fitting(x, y, y_err, x_title, y_title, model, linecolors = ["#FF9E00",
                                 colors = ["#FFD166", "#06D6A0", "#118AB2", "#073B4C", "#EF476F"], ures = 'rad'):
     
     def linear_regression_with_errors(x, y, y_err, model):
-        popt, pcov = curve_fit(model, xdata = x, ydata = y, sigma=y_err, absolute_sigma=True)
+        popt, pcov = curve_fit(model, xdata = x, ydata = y, sigma=y_err, absolute_sigma=True, bounds = [[20, 0, 0, 0, 0, 0, 0, 0, 0, 0], [50, 0.000001, 10, np.inf, 0.001, 6.283185307179586, 0.0025, 0.05, 0.1, 400]], maxfev = 10000, p0 = [30, 0.00000000000001, 1, 1, 6*10**-9, 0, 0.0015, 0.01, 0.05, 150])
         return popt, np.sqrt(np.diag(pcov))
     
     fig, (ax, ax_res) = plt.subplots(nrows=2, ncols=1, figsize=(10,7), gridspec_kw={'height_ratios': [3, 1]})
 
     x_model = np.linspace(np.min(x), 
-                        np.max(x), 4000)
+                        np.max(x), 40000)
     coeff, coeff_error = linear_regression_with_errors(x, y, y_err, model)
     ax.errorbar(x, y, yerr=y_err, fmt='o', capsize=6, capthick=1, color=colors[2], ecolor=ecolors[2], elinewidth=1, 
                     markersize=4)
--- main.py
+++ main.py
@@ -6,21 +6,21 @@ import matplotlib.pyplot as plt
 import csv
 import ast
 import webbrowser
-import time
+import time as clock
 import PIL as pillow
 from Oscillation_modelling import *
 
-start_time = time.time()
+start_time = clock.time()
 
 ########### Video tracking function ###############
 # found_circles = find_contour_mnk(video_input_path = 'Input_videos\Third_video_data_v2.mp4', video_output_path = "Output_videos\Third_test_tracking.avi", 
 #                  pos_circle_txt_path = "Position of circles\Third_test_position_of_dot.txt", Countour_finding_param = {
 #                             'dp': 1,                   # Inverse of precision (lower = higher precision)
-#                             'mindist': 16,             # Minimum distance between circle centers (px)
+#                             'mindist': 6,             # Minimum distance between circle centers (px)
 #                             'canny_threshold': 6,      # Higher = fewer edges (but more precise)
 #                             'circle_threshold': 6,     # Higher = stricter circle detection
 #                             'min_radius': 1,            # Minimum radius (px)
-#                             'max_radius': 10,            # Maximum radius (px)
+#                             'max_radius': 5,            # Maximum radius (px)
 #                             }, scale_factor = 0.25, lower_color_bdd = np.array([30, 100, 30]), 
 #                             upper_color_bdd = np.array([180, 255, 200]), morb_kernel_size = (4,4), real_time_video = True,
 #                             mouse_curser = False)
@@ -34,15 +34,19 @@ found_circles = np.asarray(found_circles)
 
 
 ######## Visualising data and finding the period ##########
-plt.plot([x/60 for x in range(len(found_circles))], found_circles[:,0])
-plt.show()
+time = [x for x in range(len(found_circles)) if x < len(found_circles)*0.995]
+planar_disp = [y for x,y in zip(range(len(found_circles)), found_circles[:,0]) if x < len(found_circles)*0.995]
+displacement_err = [y for x,y in zip(range(len(found_circles)), found_circles[:,2]) if x < len(found_circles)*0.995]
 
-fft_signal = scipy.fft.rfft(found_circles[:,0])
-fft_freq = scipy.fft.rfftfreq(len(found_circles), d=1/60)
+# plt.plot(time, planar_disp)
+# plt.show()
+
+fft_signal = np.abs(scipy.fft.rfft(planar_disp))
+fft_freq = scipy.fft.rfftfreq(len(time), d=1/60)
 
 
 idx_max, _ = scipy.signal.find_peaks(fft_signal, threshold = 10000, distance = 60, prominence = 100)
-# print(fft_freq[idx_max])
+print(fft_freq[idx_max])
 plt.plot(fft_freq, fft_signal, color = 'gray')
 plt.scatter(fft_freq[idx_max], fft_signal[idx_max], color = 'orange', marker = 'x')
 plt.show()
@@ -50,13 +54,13 @@ plt.show()
 
 
 ##### Curve fit #####
-time, planar_disp = [x/60 for x in range(len(found_circles))], found_circles[:,0]
-coeff, coeff_err = curve_fitting(time, planar_disp, y_err = found_circles[:,2], x_title = 'Time (s)', y_title = 'Planar displacement (px)', model = torsional_oscillator_model)
-for c,e in coeff, coeff_err :
+
+coeff, coeff_err = curve_fitting(time, planar_disp, y_err = displacement_err, x_title = 'Time (s)', y_title = 'Planar displacement (px)', model = torsional_oscillator_model)
+for c,e in zip(coeff, coeff_err) :
     print(c, '+/-', e)
 
 
 print()
-print("--- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (clock.time() - start_time))
 
 
